43c868764a244b8ca6e869fb51fef2c690350b3c
Ryan Guelzo <ryan.t.guelzo.ctr@mail.mil>
2024-03-18 09:58:05 -0700

    Setting up Controller functions.

--- src/iroha/iroha.controller.ts
+++ src/iroha/iroha.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('iroha')
 export class IrohaController {
   @Get()
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Get('/transactions')
+  viewAllTransactions(): string {
+    return 'all!';
+  }
+
+  @Get('/accounts/')
+  getAllAccounts(): string {
+    return 'All Accounts';
+  }
+
+  @Get('/accounts/:accountId')
+  getAccountDetails(@Param(':accountId') accountId: string): string {
+    return `Account ${accountId}`;
   }
 }
