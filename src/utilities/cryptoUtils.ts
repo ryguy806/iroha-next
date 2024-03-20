a54d8b30737c9c7f743add816741e95443381603
Ryan Guelzo <ryan.t.guelzo.ctr@mail.mil>
2024-03-20 10:54:43 -0700

    Setting up the client.

--- src/utilities/cryptoUtils.ts
+++ src/utilities/cryptoUtils.ts
@@ -1,10 +1,19 @@
-import { Signer } from '@iroha2/client';
+import {
+  Client,
+  Signer,
+  ToriiRequirementsForApiHttp,
+  ToriiRequirementsForApiWebSocket,
+} from '@iroha2/client';
 import { crypto } from '@iroha2/crypto-target-node';
 import {
   DomainId,
   AccountId,
+  Executable,
 } from '@iroha2/data-model/dist/types/__generated__.d';
+import { fetch } from 'undici';
+import { adapter as WS } from '@iroha2/client/dist/web-socket/node';
 
+//Client information
 const keyPair = crypto.KeyPair.fromJSON({
   public_key: process.env.IROHA_PUBLIC_KEY,
   private_key: {
@@ -21,3 +30,19 @@ const accountId = AccountId({
 });
 
 export const signer = new Signer(accountId, keyPair);
+
+//Torii requirements
+export const toriiRequirements: ToriiRequirementsForApiHttp &
+  ToriiRequirementsForApiWebSocket = {
+  apiURL: 'http://127.0.0.1:8080',
+  ws: WS,
+  fetch: fetch.bind(window),
+};
+
+export const client = new Client({ signer });
+
+declare const exec: Executable;
+
+export const clientConfing = async () => {
+  await client.submitExecutable(toriiRequirements, exec);
+};
